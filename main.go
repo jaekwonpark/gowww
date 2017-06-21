4d5dfdbbd6752b346f05de037a6eb80a1ba13d1c
Jaekwon Park <jaekwon.park@couchbase.com>
2017-06-20 21:33:02 -0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"github.com/stianeikeland/go-rpio"
+	"os"
+	"time"
+)
+
+var (
+	pin = rpio.Pin(22)
+)
+
+func main() {
+
+	// Open and map memory to access gpio, check for errors
+	if err := rpio.Open(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Unmap gpio memory when done
+	defer rpio.Close()
+
+	// Set pin to output mode
+	pin.Output()
+
+	// Toggle pin 20 times
+	for x := 0; x < 20; x++ {
+		pin.Toggle()
+		time.Sleep(time.Second / 5)
+	}
+
+}
+
