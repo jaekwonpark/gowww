27b2e53df08582282a12d3a87f2a487218955385
Jaekwon Park <jkpark@gmail.com>
2017-06-28 16:47:20 +0000

    code cleanup

--- main.go
+++ main.go
@@ -2,17 +2,68 @@ package main
 
 import (
 	"fmt"
-	"github.com/stianeikeland/go-rpio"
 	"os"
+	"github.com/stianeikeland/go-rpio"
 	"time"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
+	"net/http"
+	"log"
 )
 
 var (
-	waterStations = []int{24,25}
+	waterStations = []int{22, 23, 24}
+	Garage = 25
+	store = sessions.NewCookieStore([]byte(os.Getenv("COOKIE_SESSION")))
+	webkey = os.Getenv("WEBKEY")
+	cert = os.Getenv("CERT_PATH")+"bundle.crt"
+	certkey = os.Getenv("CERT_PATH")+"private.key"
+	cookieName = "ctrl"
+	sessionName = "certified"
 )
+
+func ctrl(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, cookieName)
+
+	if auth, ok := session.Values[sessionName].(string); !ok || (auth != webkey) {
+		http.Error(w, "Forbidden", http.StatusForbidden)
+		return
+	}
+	fmt.Fprintln(w, "you are allowed")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, cookieName)
+	session.Values[sessionName] = webkey
+	session.Save(r,w)
+}
+
+func logout(w http.ResponseWriter, r *http.Request) {
+	session, _ := store.Get(r, cookieName)
+	session.Values[sessionName] = ""
+	session.Save(r,w)
+}
 
 func main() {
 
+
+	// declare mux router
+	r := mux.NewRouter()
+
+	r.HandleFunc("/ctrl", ctrl)
+	//r.HandleFunc("/login", login)
+	r.HandleFunc("/logout", logout)
+
+
+
+
+	err := http.ListenAndServeTLS(":443", cert, certkey, r)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+
+
+	
 	// Open and map memory to access gpio, check for errors
 	if err := rpio.Open(); err != nil {
 		fmt.Println(err)
@@ -23,10 +74,12 @@ func main() {
 	defer rpio.Close()
 
 	// sprinkler
-	turnOn(waterStations, 5, 1)
+	//turnOn(waterStations, 5, 1)
+	//turnOn(waterStations, 5, 1)
 
 	// garage
-	toggle(24, 5)
+	//toggle(Garage, 5)
+	//toggle(Garage, 5)
 
 }
 
@@ -35,9 +88,9 @@ func turnOn(pins []int, min time.Duration, sleep time.Duration) {
 		pin := rpio.Pin(v)
 		pin.Output()
 		pin.Low()
-		time.Sleep(time.Minute*min)
+		time.Sleep(time.Second*min)
 		pin.High()
-		time.Sleep(time.Minute*sleep)
+		time.Sleep(time.Second*sleep)
 	}
 }
 
@@ -48,4 +101,3 @@ func toggle(pinNo int, sec time.Duration) {
 	time.Sleep(time.Second*sec)
 	pin.High()
 }
-
